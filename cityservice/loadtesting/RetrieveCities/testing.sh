52d11ca8489536ba5e2a092e89ea4901b8567390
huangchaogang <huangchaogang@4paradigm.com>
2020-06-13 15:52:07 +0800

    load testing

--- cityservice/loadtesting/RetrieveCities/testing.sh
+++ cityservice/loadtesting/RetrieveCities/testing.sh
@@ -1,5 +1,5 @@
 service_dir="$(cd ../.. && pwd)"
 
 for casefile in "${service_dir}"/loadtesting/RetrieveCities/*.json; do
-  ghz --insecure --proto="${service_dir}"/proto/cityservice.proto -O html --call proto.CityService.RetrieveCities 127.0.0.1:50051 -c 100 -n 2000 -D "${casefile}"
+  ghz --insecure --proto="${service_dir}"/proto/cityservice.proto --call proto.CityService.RetrieveCities 127.0.0.1:50051 -c 100 -n 2000 -D "${casefile}"
 done
